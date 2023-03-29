d514eb8c1a4f1fc048acc78e518970a0e0aded01
OlojaMuyiwa <olojam4969@gmail.com>
2023-03-29 13:44:31 -0700

    fixed

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+"""Define a class Square."""
+
 
 class Square:
-    """ A square """
+    """Represent a square."""
     pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
+"""Define a class Square."""
+
 
 class Square:
+    """Represent a square."""
 
     def __init__(self, size):
-        """ A constructer that initialise the data type """
-
-        """ Takes an argument 'size' """
+        """Initialize a new Square.
+        Args:
+            size (int): The size of the new square.
+        """
         self.__size = size
