5a5ec18b000097a50c87f485801f3b2458a88109
OlojaMuyiwa <olojam4969@gmail.com>
2023-03-29 13:41:28 -0700

    constructor and private data type

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+class Square:
+
+    def __init__(self, size):
+        """ A constructer that initialise the data type """
+
+        """ Takes an argument 'size' """
+        self.__size = size
