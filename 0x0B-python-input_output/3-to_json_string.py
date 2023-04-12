e47bfa8a00fdb4b93426f5f4770ff0052ca9f0ea
OlojaMuyiwa <olojam4969@gmail.com>
2023-04-12 05:12:35 -0700

    string to JSON

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!usrr/bin/python3
+"""Defines a string-to-JSON function."""
+import json
+
+
+def to_json_string(my_obj):
+    """Returns the json representation of a string object."""
+    return json.dumps(my_obj)
